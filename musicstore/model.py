b372ae055fd501310926c83d4f3a42233bf27378
Pedro Hernandez <phernandez804@soyudemedellin.edu.co>
2024-08-29 12:50:54 -0500

    class disc

--- musicstore/model.py
+++ musicstore/model.py
@@ -4,4 +4,17 @@ class transaction:
     self: int = 1
     Suply: int = 2
     date: datetime() # type: ignore
-    def _init_(type: int, copies: int):
+    def _init_(type: int, copies: int)
+class disc:
+    def _init_(sid: str, title: str, artist: str, sale_price: float, purchase_price: float, quantity: int):
+        sid: str
+        title: str
+        artist: str
+        sale_price: float
+        purchase_price: float
+        quantity: int
+        transactions: list[transaction]= {}
+        song_list: list[str]= {}
+        add_song:(song: str)
+
+
